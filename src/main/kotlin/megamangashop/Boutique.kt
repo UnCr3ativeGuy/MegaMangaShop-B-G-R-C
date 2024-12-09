9489f32e7e4c87474673c8096fe96987e00c3cc6
AdrienBelloc <adrien82200@gmail.com>
2024-12-09 15:47:02 +0100

    Calcul prixTTC

--- src/main/kotlin/megamangashop/Boutique.kt
+++ src/main/kotlin/megamangashop/Boutique.kt
@@ -1,15 +1,20 @@
 package org.example.montaine.guillaume.megamangashop
 
-
-data class Boutique(val Pays: String) {
-    fun selectionResidence(Pays: String) : Float{
-        return when(Pays){
+data class Boutique(val pays: String) {
+    fun getTVA() : Double{
+        return when(pays){
             "France"->0.2
             "Espagne"->0.182
             "Allemagne"->0.156
             "Royaume-Uni"-> 0.228
             "Belgique" -> 0.17
+            else -> 0.0
         }
+    }
+
+    fun calculerPrixTTC(prix:Double):Double {
+        val tva = getTVA()
+        return prix * (tva+1)
     }
 }
 
