d452e2024db4f1437c2201f14fe10831da19ad39
AdrienBelloc <adrien82200@gmail.com>
2024-12-09 15:57:17 +0100

    Déplacement des fonctions dans boutique

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,16 @@
 
 package org.example.montaine.guillaume
 
-import org.example.montaine.guillaume.megamangashop.Commande
+import org.example.montaine.guillaume.megamangashop.Boutique
 import org.example.montaine.guillaume.megamangashop.Manga
 
 fun main() {
     var exitInput = ""
-    val c = Commande()
+    val b = Boutique("")
     while(exitInput != "Q" || exitInput != "q") {
+        println("Entrez un pays de livraison")
+        val paysInput = readln()
+        val b = Boutique(paysInput)
         println("Entrez le nom du manga")
         val nomInput = readln()
         println("Entrez le prix du manga")
@@ -16,9 +19,9 @@ fun main() {
         val qteInput = readln()
         val prix = prixInput.toDouble()
         val qte = qteInput.toInt()
-        c.addMangas(nomInput, prix, qte)
+        b.addMangas(nomInput, prix, qte)
         println("Si vous avez terminer appuez sur Q")
         exitInput = readln()
     }
-    println(c.calculPrix())
+    println(b.calculPrix())
 }
--- src/main/kotlin/megamangashop/Boutique.kt
+++ src/main/kotlin/megamangashop/Boutique.kt
@@ -1,6 +1,25 @@
 package org.example.montaine.guillaume.megamangashop
 
 data class Boutique(val pays: String) {
+    private var mangas = mutableMapOf<Manga, Int>()
+
+    fun addMangas(nomManga:String, prixManga:Double, qte:Int) {
+        assert(qte<=0) {"La quantité doit être supérieur à 0"}
+        val manga = Manga(nomManga, prixManga)
+        if (mangas.containsKey(manga)) {
+            var qteAncienne = mangas.get(manga)!!
+            qteAncienne += qte
+            mangas.put(manga, qteAncienne)
+        } else {
+            mangas.put(manga, qte)
+        }
+    }
+
+    fun getQuantite(manga:Manga): Int {
+        assert(mangas.containsKey(manga)) {"Ce manga n'existe pas"}
+        return mangas.get(manga)!!
+    }
+
     fun getTVA() : Double{
         return when(pays){
             "France"->0.2
@@ -15,6 +34,15 @@ data class Boutique(val pays: String) {
     fun calculerPrixTTC(prix:Double):Double {
         val tva = getTVA()
         return prix * (tva+1)
+    }
+
+    fun calculPrix(): Double {
+        var total = 0.0
+        mangas.forEach {
+            total += it.value * it.key.getPrix()
+        }
+        val totalTTC = calculerPrixTTC(total)
+        return totalTTC
     }
 }
 
