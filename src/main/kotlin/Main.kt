d9210be553cee1623de653eeafb48f1211eb8fd2
AdrienBelloc <adrien82200@gmail.com>
2024-12-09 15:27:53 +0100

    Modification du main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,12 @@
 
 package org.example.montaine.guillaume
 
+import org.example.montaine.guillaume.megamangashop.Commande
+import org.example.montaine.guillaume.megamangashop.Manga
+
 fun main() {
-    println("Hello World!")
+    val c = Commande()
+    c.addMangas("Manga2", 3.92, 42)
+    c.addMangas("Manga3", 4.10, 82)
+    println(c.calculPrix())
 }
