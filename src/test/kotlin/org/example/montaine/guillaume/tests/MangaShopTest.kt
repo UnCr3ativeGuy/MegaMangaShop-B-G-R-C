a443d9499fe6eb6e01f85f416b5beb52e91f6410
sofianghobrini <sofiangho31000@outlook.fr>
2024-12-09 15:02:23 +0100

    context si le client selectionne le pays Royaume-Uni

--- src/test/kotlin/org/example/montaine/guillaume/tests/MangaShopTest.kt
+++ src/test/kotlin/org/example/montaine/guillaume/tests/MangaShopTest.kt
@@ -4,45 +4,96 @@ package org.example.montaine.guillaume.tests
 import io.kotest.core.spec.style.BehaviorSpec
 
 class MangaShopTest: BehaviorSpec({
-
-    context("je veux vérifier les reductions par état et selon les montants") {
-
-    }
     context("Je veux verifier la TVA quand le client selectionne la france") {
-        given("Le client selection le pays"){
+        given("Le client selection le pays") {
             val ticket = TicketDeCaisse()
 
-            When("Le client selection son pays de résidence la France"){
+            When("Le client selection son pays de résidence la France") {
                 var command = ticket.selectionPays("France")
             }
-            then("L'affichage de la TVA du pays correspondante"){
+            then("L'affichage de la TVA du pays correspondante") {
                 command shouldBe 0.2
             }
         }
     }
     context("Je veux verifier la TVA quand le client selectionne l'espagne") {
-        given("Le client selection le pays"){
+        given("Le client selection le pays") {
             val ticket = TicketDeCaisse()
 
-            When("Le client selection son pays de résidence Espagne"){
+            When("Le client selection son pays de résidence Espagne") {
                 var command = ticket.selectionPays("Espagne")
             }
-            then("L'affichage de la TVA du pays correspondante"){
+            then("L'affichage de la TVA du pays correspondante") {
                 command shouldBe 0.182
             }
         }
     }
     context("Je veux verifier la TVA quand le client selectionne l'Allemagne") {
-        given("Le client selection le pays"){
+        given("Le client selection le pays") {
             val ticket = TicketDeCaisse()
 
-            When("Le client selection son pays de résidence Allemagne"){
+            When("Le client selection son pays de résidence Allemagne") {
                 var command = ticket.selectionPays("Allemagne")
             }
-            then("L'affichage de la TVA du pays correspondante"){
+            then("L'affichage de la TVA du pays correspondante") {
                 command shouldBe 0.156
             }
         }
     }
+    context("Je veux verifier la TVA quand le client selectionne l'Allemagne") {
+        given("Le client selection le pays") {
+            val ticket = TicketDeCaisse()
 
+            When("Le client selection son pays de résidence Allemagne") {
+                var command = ticket.selectionPays("Allemagne")
+            }
+            then("L'affichage de la TVA du pays correspondante") {
+                command shouldBe 0.156
+            }
+        }
+    }
+    context("Je veux verifier la TVA quand le client selectionne le Royaume-Uni") {
+        given("Le client selection le pays") {
+            val ticket = TicketDeCaisse()
+
+            When("Le client selection son pays de résidence Royaume-Uni") {
+                var command = ticket.selectionPays("Royaume-Uni")
+            }
+            then("L'affichage de la TVA du pays correspondante") {
+                command shouldBe 0.228
+            }
+        }
+    }
+    context("Je veux verifier la TVA quand le client selectionne le Royaume-Uni") {
+        given("Le client selection le pays") {
+            val ticket = TicketDeCaisse()
+
+            When("Le client selection son pays de résidence Royaume-Uni") {
+                var command = ticket.selectionPays("Royaume-Uni")
+            }
+            then("L'affichage de la TVA du pays correspondante") {
+                command shouldBe 0.228
+            }
+        }
+    }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
