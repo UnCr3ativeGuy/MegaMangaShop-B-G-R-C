5952b7cf3ac7d56b8ef4ec89e32ed4f4c5e689a2
sofianghobrini <sofiangho31000@outlook.fr>
2024-12-09 14:46:00 +0100

    context si le client selectionne le pays la france

--- src/test/kotlin/org/example/montaine/guillaume/tests/MontaineGuillaumeTest.kt
+++ src/test/kotlin/org/example/montaine/guillaume/tests/MontaineGuillaumeTest.kt
@@ -7,4 +7,19 @@ class MontaineGuillaumeTest: BehaviorSpec({
 
     context("je veux vérifier les reductions par état et selon les montants") {
 
-    }})
+    }
+    context("Je veux verifier la TVA quand le client selectionne la france") {
+        given("Le client selection le pays"){
+            val ticket = TicketDeCaisse("France")
+
+            When("Le client selection son pays de résidence par exemple la France"){
+                var command = ticket.selectionPays("France")
+            }
+            then("L'affichage de la TVA du pays correspondante"){
+                command shouldBe 0.2
+            }
+        }
+    }
+
+
+})
