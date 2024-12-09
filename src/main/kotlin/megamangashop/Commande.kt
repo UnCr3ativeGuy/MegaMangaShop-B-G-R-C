428c488bacf427354016e47d0540f26af8113f97
AdrienBelloc <adrien82200@gmail.com>
2024-12-09 14:54:46 +0100

    Prise en compte d'un manga déjà existant et d'une quantité à 0

--- src/main/kotlin/megamangashop/Commande.kt
+++ src/main/kotlin/megamangashop/Commande.kt
@@ -5,6 +5,13 @@ class Commande {
     private var mangas = mutableMapOf<String, Int>()
 
     fun addMangas(manga:String, qte:Int) {
-        mangas.put(manga, qte)
+        assert(qte<=0) {"La quantité doit être supérieur à 0"}
+        if (mangas.containsKey(manga)) {
+            var qteAncienne = mangas.get(manga)!!
+            qteAncienne += qte
+            mangas.put(manga, qteAncienne)
+        } else {
+            mangas.put(manga, qte)
+        }
     }
 }
