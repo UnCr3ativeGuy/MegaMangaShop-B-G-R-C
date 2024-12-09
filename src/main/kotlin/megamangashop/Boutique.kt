ed9c81824da3dd597329ed05a0dd1dac36b29681
PLC <pierrelouischaux@gmail.com>
2024-12-09 15:54:41 +0100

    calcule prixTot avec taxe dans Boutique

--- src/main/kotlin/megamangashop/Boutique.kt
+++ src/main/kotlin/megamangashop/Boutique.kt
@@ -1,16 +1,34 @@
 package org.example.montaine.guillaume.megamangashop
 
-enum class Pays(){FRANCE, ESPAGNE, ROYAUME-UNI, ALLEMAGNE, BELGIQUE}
-
 data class Boutique(val Pays: String) {
-    fun selectionResidence(Pays: String) : float{
-        return when(Pays){
-            "France"->0.2
-            "Espagne"->0.182
-            "Allemagne"->0.156
-            "Royaume-Uni"-> 0.228
-            "Belgique" -> 0.17
+
+    fun calculeRemise(prix: Double): Double {
+        val remise: Double =when  {
+            prix in 150.0 .. 199.0 -> 2.0
+            prix in 200.0..299.0 -> 3.0
+            prix in 300.0..499.0 -> 5.0
+            prix in 500.0..999.0 -> 7.0
+            prix > 1000.0 -> 10.0
+            else -> 0.0 // Pas de remise
         }
+        return remise
+    }
+
+
+
+    fun calculePrixTot(prix: Double,Pays:String,remise:Double): Double {
+        var prixTot =0.0
+        var taxe: Double = when (Pays) {
+            "France" -> 1.2
+            "Espagne"-> 1.182
+            "Allemagne"->1.156
+            "Royaume-Uni"->1.228
+            "Belgique"-> 1.17
+            else -> 0.0
+        }
+
+        prixTot= (prix*remise)*taxe
+        return  prixTot
     }
 }
 
